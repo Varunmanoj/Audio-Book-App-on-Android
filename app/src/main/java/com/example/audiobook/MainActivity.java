4ddf63108f4bcbf71b5b5d9b4dd84692bb312029
Varun Kumar <varun.kumar@xaviers.edu.in>
2022-01-22 19:56:50 +0530

    Fixed not speaking issue for TTS on main Activity

--- app/src/main/java/com/example/audiobook/MainActivity.java
+++ app/src/main/java/com/example/audiobook/MainActivity.java
@@ -26,54 +26,60 @@ public class MainActivity extends AppCompatActivity {
 
 //        Create on click for list item
         Bookname.setOnItemClickListener((parent, view, position, id) -> {
-//Extract the text selected  by user in string
-            String selected = Bookname.getItemAtPosition(position).toString();
-            //Check  for Accessibility
-            AccessibilityManager am = (AccessibilityManager) getSystemService(ACCESSIBILITY_SERVICE);
 
-            //CHeck  if TalkBack Screen Reader is already Running
-
+//            Extract the text selected  by user in string
 //                Make the app self voicing
-                String Readtext = Bookname.getItemAtPosition(position).toString();
+            String Readtext = Bookname.getItemAtPosition(position).toString();
 
-                mTTS = new TextToSpeech(getApplicationContext(), status -> {
-                    if (status == TextToSpeech.SUCCESS) {
-                        mTTS.setLanguage(Locale.ENGLISH);
-                    }
-//                    Speak button click
-                    mTTS.setSpeechRate(0.95F);
-                    mTTS.speak(Readtext,TextToSpeech.QUEUE_FLUSH,null);
-
-                });
-//                Create List Items  clickable
-
-                switch (position) {
-                    case 0:
-                        Toast.makeText(getApplicationContext(), "You selected book 1", Toast.LENGTH_SHORT).show();
-                        Intent nextscreanIntent = new Intent(MainActivity.this, Book1Chapters.class);
-                        startActivity(nextscreanIntent);
-                        break;
-                    case 1:
-                        Toast.makeText(getApplicationContext(), "You selected book 2", Toast.LENGTH_SHORT).show();
-                        Intent Book2Intent = new Intent(MainActivity.this, Book2ChapterList.class);
-                        startActivity(Book2Intent);
-                        break;
+            mTTS = new TextToSpeech(getApplicationContext(), status -> {
+                if (status == TextToSpeech.SUCCESS) {
+                    mTTS.setLanguage(Locale.ENGLISH);
                 }
-            });
-        }
+//                    Speak button click
+                mTTS.speak(Readtext,TextToSpeech.QUEUE_FLUSH,null);
 
+            });
+
+//                Create List Items  clickable
+            switch (position) {
+                case 0:
+                    Toast.makeText(getApplicationContext(), "You selected book 1", Toast.LENGTH_SHORT).show();
+                    Intent nextscreanIntent = new Intent(MainActivity.this, Book1Chapters.class);
+                    startActivity(nextscreanIntent);
+                    break;
+                case 1:
+                    Toast.makeText(getApplicationContext(), "You selected book 2", Toast.LENGTH_SHORT).show();
+                    Intent Book2Intent = new Intent(MainActivity.this, Book2ChapterList.class);
+                    startActivity(Book2Intent);
+                    break;
+            }
+        });
+    }
 
     @Override
     protected void onStop() {
         super.onStop();
+//                Release resources if audio tts is not speaking
         mTTS.stop();
         mTTS.shutdown();
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+//        Release resources if audio tts is not speaking
+        if (!mTTS.isSpeaking()){
+            mTTS.stop();
+        }
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        mTTS.stop();
-        mTTS.shutdown();
+//                Release resources if audio tts is not speaking
+        if (!mTTS.isSpeaking()) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
     }
 }
